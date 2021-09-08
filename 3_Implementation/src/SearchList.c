77ee5327c62d5a3e5294ed891186e22aede38de3
PrakashReddyPadala <89823755+PrakashReddyPadala@users.noreply.github.com>
2021-09-08 18:13:48 +0530

    Update SearchList.c

--- 3_Implementation/src/SearchList.c
+++ 3_Implementation/src/SearchList.c
@@ -1,13 +1,4 @@
-/**
- * @file SearchList.c
- * @author yukti yukti.patel@ltts.com
- * @brief 
- * @version 0.1
- * @date 2021-05-25
- * 
- * @copyright Copyright (c) 2021
- * 
- */
+
 #include<string.h>
 #include<stdio.h>
 #include"../inc/Employee.h"
