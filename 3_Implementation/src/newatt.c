963f15bbbafb47db3dbdc9191ebf0e6d6b3fc80d
PrakashReddyPadala <89823755+PrakashReddyPadala@users.noreply.github.com>
2021-09-08 18:17:28 +0530

    Update newatt.c

--- 3_Implementation/src/newatt.c
+++ 3_Implementation/src/newatt.c
@@ -1,13 +1,4 @@
-/**
- * @file newatt.c kavya yukti shriya
- * @author your name (you@domain.com)
- * @brief 
- * @version 0.1
- * @date 2021-05-25
- * 
- * @copyright Copyright (c) 2021
- * 
- */
+
 #include "../inc/Employee.h"
 #include <malloc.h>
 #include <stdlib.h>
