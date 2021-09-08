d99f9f96439d91aafa1879d83c2ab1cb85a6ba4f
PrakashReddyPadala <89823755+PrakashReddyPadala@users.noreply.github.com>
2021-09-08 18:18:08 +0530

    Update main_test_search.c

--- 3_Implementation/test/main_test_search.c
+++ 3_Implementation/test/main_test_search.c
@@ -1,13 +1,4 @@
-/**
- * @file main_test_search.c
- * @author yukti 
- * @brief 
- * @version 0.1
- * @date 2021-05-25
- * 
- * @copyright Copyright (c) 2021
- * 
- */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "test_search.h"
